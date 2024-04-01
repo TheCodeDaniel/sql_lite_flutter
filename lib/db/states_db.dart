2cc5c310d2bf7a99681807a2087f506c149d1fe3
Daniel <thecodedaniel@gmail.com>
2024-04-01 10:39:24 +0100

    first commit

---
+++ lib/db/states_db.dart
@@ -0,0 +1,66 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:untitled/db/database_service.dart';
+import 'package:untitled/model/states_model.dart';
+
+class StatesDB {
+  final tableName = "state";
+
+  Future<void> createTable(Database database) async {
+    await database.execute(""" CREATE TABLE IF NOT  EXISTS $tableName (
+      "id" INTEGER NOT NULL,
+      "stateName" TEXT NOT NULL,
+      PRIMARY KEY("id", AUTOINCREMENT )
+    ) """);
+  }
+
+  // create
+  Future<int> create({required String stateName}) async {
+    final database = await DatabaseService().database;
+    return await database.rawInsert(
+      '''INSERT INTO $tableName (stateName) VALUES (?)''',
+      [stateName],
+    );
+  }
+
+  // read
+  Future<List<States>> fetchAll() async {
+    final database = await DatabaseService().database;
+    final states = await database.rawQuery(
+      '''SELECT * FROM $tableName''',
+    );
+    return states.map((data) => States.fromSqfliteDatabase(data)).toList();
+  }
+
+  // read by id
+  Future<States> fetchById(int id) async {
+    final database = await DatabaseService().database;
+    final states = await database.rawQuery(
+      '''SELECT * from $tableName WHERE id = ?''',
+      [id],
+    );
+    return States.fromSqfliteDatabase(states.first);
+  }
+
+  // update
+  Future<int> update({required int id, String? stateName}) async {
+    final database = await DatabaseService().database;
+    return await database.update(
+      tableName,
+      {
+        if (stateName != null) 'stateName': stateName,
+      },
+      where: 'id = ?',
+      conflictAlgorithm: ConflictAlgorithm.rollback,
+      whereArgs: [id],
+    );
+  }
+
+  // delete
+  Future<void> delete(int id) async {
+    final database = await DatabaseService().database;
+    await database.rawDelete(
+      '''DELETE FROM $tableName WHERE id = ?''',
+      [id],
+    );
+  }
+}
---
+++ lib/model/states_model.dart
@@ -0,0 +1,14 @@
+class States {
+  final int id;
+  final String stateName;
+
+  States({
+    required this.id,
+    required this.stateName,
+  });
+
+  factory States.fromSqfliteDatabase(Map<String, dynamic> map) => States(
+        id: map['id'] ?? 0,
+        stateName: map['title'] ?? '',
+      );
+}
